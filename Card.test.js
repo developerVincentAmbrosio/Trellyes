0b434789631ddd7bcd4491514eda1b0eacd62c9f
developerVincentAmbrosio <70174185+developerVincentAmbrosio@users.noreply.github.com>
2020-12-08 09:44:22 -0500

    Add files via upload
    test files for List and Card components added

---
+++ Card.test.js
@@ -0,0 +1,30 @@
+// SMOKE TEST
+
+// make React available
+import React from 'react';
+
+// make the ReactDOM available, necessary for rendering the component
+import ReactDOM from 'react-dom';
+
+// make the App component available
+import App from './Card';
+
+// this is the test case
+it('renders without crashing', () => {
+  // first create a DOM element to render the component into
+  const div = document.createElement('div');
+
+  // render the component, this is the actual test, if something is wrong it will fail here
+  ReactDOM.render(<App />, div);
+
+  // clean up code
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+//SNAPSHOT TEST
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Card />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
